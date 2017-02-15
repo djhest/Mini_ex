45664e83dac365b8437acad9478e81eeeb5c5db3
djhest <jdemuth18@gmail.com>
2017-02-15 18:12:02 +0100

    Changed the colors

--- mini_ex2/sketch.js
+++ mini_ex2/sketch.js
@@ -1,4 +1,8 @@
-
+var col = {
+  r : 0,
+  g : 0,
+  b : 0
+};
 
 function setup() {
   createCanvas(displayWidth,displayHeight);
@@ -6,91 +10,97 @@ function setup() {
 }
 
 function draw() {
-  background(0);
+  col.r = random(0,126);
+  col.g = random(127,255);
+  background(col.r);
   
   noStroke();
   
-  fill(255);
+  fill(col.g);
   rect(0,25,windowWidth,windowHeight);
-  fill(0);
+  fill(col.r);
   rect(0,50,windowWidth,windowHeight);
-  fill(255);
+  fill(col.g);
   rect(0,75,windowWidth,windowHeight);
-  fill(0);
+  fill(col.r);
   rect(0,100,windowWidth,windowHeight);
-  fill(255);
+  fill(col.g);
   rect(0,125,windowWidth,windowHeight);
-  fill(0);
+  fill(col.r);
   rect(0,150,windowWidth,windowHeight);
-  fill(255);
+  fill(col.g);
   rect(0,175,windowWidth,windowHeight);
-  fill(0);
+  fill(col.r);
   rect(0,200,windowWidth,windowHeight);
-  fill(255);
+  fill(col.g);
   rect(0,225,windowWidth,windowHeight);
-  fill(0);
+  fill(col.r);
   rect(0,250,windowWidth,windowHeight);
-  fill(255);
+  fill(col.g);
   rect(0,275,windowWidth,windowHeight);
-  fill(0);
+  fill(col.r);
   rect(0,300,windowWidth,windowHeight);
-  fill(255);
+  fill(col.g);
   rect(0,325,windowWidth,windowHeight);
-  fill(0);
+  fill(col.r);
   rect(0,350,windowWidth,windowHeight);
-  fill(255);
+  fill(col.g);
   rect(0,375,windowWidth,windowHeight);
-  fill(0);
+  fill(col.r);
   rect(0,400,windowWidth,windowHeight);
-  fill(255);
+  fill(col.g);
   rect(0,425,windowWidth,windowHeight);
-  fill(0);
+  fill(col.r);
   rect(0,450,windowWidth,windowHeight);
-  fill(255);
+  fill(col.g);
   rect(0,475,windowWidth,windowHeight);
-  fill(0);
+  fill(col.r);
   rect(0,500,windowWidth,windowHeight);
-  fill(255);
+  fill(col.g);
   rect(0,525,windowWidth,windowHeight);
-  fill(0);
+  fill(col.r);
   rect(0,550,windowWidth,windowHeight);
-  fill(255);
+  fill(col.g);
   rect(0,575,windowWidth,windowHeight);
-  fill(0);
+  fill(col.r);
   rect(0,600,windowWidth,windowHeight);
-  fill(255);
+  fill(col.g);
   rect(0,625,windowWidth,windowHeight);
-  fill(0);
+  fill(col.r);
   rect(0,650,windowWidth,windowHeight);
-  fill(255);
+  fill(col.g);
   rect(0,675,windowWidth,windowHeight);
-  fill(0);
+  fill(col.r);
   rect(0,700,windowWidth,windowHeight);
   
    // hoved 1
+   col.r = random(0,255);
+   col.g = random(0,255);
+   col.b = random(0,255);
+   
   strokeWeight(5);
-  stroke(88,75,83);
-  fill(228,187,151);
+  stroke(col.r,col.g,col.b);
+  fill(col.r,col.g,col.b);
   ellipse(mouseX,mouseY,100,100);
   
   strokeWeight(2);
   stroke(0);
-  fill(255);
+  fill(col.r);
   ellipse(mouseX,mouseY-20,40,40);
   
   strokeWeight(3);
-  stroke(97,192,255);
-  fill(0);
+  stroke(col.r,col.g,col.b);
+  fill(col.r);
   ellipse(mouseX,mouseY-20,10,10);
   
-  stroke(210,92,99);
+  stroke(col.r,col.g,col.b);
   triangle(mouseX-20,mouseY+10,mouseX+20,mouseY+10,mouseX,mouseY+40);
   
   //hoved 2
   
   if (mouseIsPressed) {
     
-     background(255,WEBGL);
+     background(255);
   
   noStroke();
   
