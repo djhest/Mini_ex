b703d82d44799521f980c4a3ef28fbc4a3762f18
djhest <jdemuth18@gmail.com>
2017-03-20 13:34:41 +0100

    Update sketch.js

--- Mini_ex6/sketch.js
+++ Mini_ex6/sketch.js
@@ -1,7 +1,7 @@
-var n = 400;
-var minRad = 1;
-var maxRad = 300;
-var Ang = 0.05;
+var n = 400; //Number of particles in every vertex
+var minRad = 1; // minimum radius of the vertex
+var maxRad = 300; // maximum radius of the vertex
+var Ang = 0.05; // these to variables decides the outcome of the vertex. The higher the number, the more smooth looking it becomes.
 var Time = 0.005;
 
 function setup() {
@@ -14,13 +14,13 @@ function setup() {
 function draw() {
   translate(width/2, height/2);
   rotate(85);
-  beginShape();
+  beginShape(); //From here and down to endShape(), the shape is defined
   for (var i=0; i<n; i++) {
-    var ang = map(i, 0, n, 0, TAU);
-    var rad = map(noise(i*Ang, frameCount*Time), 0, 1, minRad, maxRad);
-    var x = rad * cos(ang);
+    var ang = map(i, 0, n, 0, TAU); //This one decides how long the vertex is and TAU is another way to use TWO_PI. It gives the same result and it means that the vertex will be in a circle like shape
+    var rad = map(noise(i*Ang, frameCount*Time), 0, 1, minRad, maxRad); //This is defining the length of the radius between 1 and 300
+    var x = rad * cos(ang); // These are the placement of the vertex
     var y = rad * sin(ang);
-    curveVertex(x, y);
+    curveVertex(x, y); //The curveVertex function draws a curve between different points and can only be used inside the beginShape and endShape
 
   }
   endShape();
