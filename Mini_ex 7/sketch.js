01492de82931fb41e899ec2e9b02aac2586ccad3
djhest <jdemuth18@gmail.com>
2017-03-27 12:46:07 +0200

    Update sketch.js

--- Mini_ex 7/sketch.js
+++ Mini_ex 7/sketch.js
@@ -10,7 +10,7 @@ var kage;
 var cols = 40;
 var rows = 30;
 
-function toggleSong() {
+function toggleSong() { // A function to toggle whether the song and video is playing.
   if (song.isPlaying()) {
     song.pause();
     gif.pause();
@@ -29,15 +29,15 @@ function setup() {
   createCanvas(700,500)
 
   gif.position(200,200);
-  button = createButton('toggle');
-  button.mousePressed(toggleSong);
+  button = createButton('toggle'); //The toggle button
+  button.mousePressed(toggleSong); //What happens when the button is pressed
   button.position(100,100);
   song.play();
   gif.loop();
 
-  fft = new p5.FFT(0.9,128);
+  fft = new p5.FFT(0.9,128); //A p5 function that analyze an audio file and delivers values that can be used.
   translate(-290,0);
-  for (var i = 0; i < 140; i += 20) {
+  for (var i = 0; i < 140; i += 20) { //For loop that creates 7 sliders. They're 200px wide and rotated 90 degrees so that they are vertical
     sliders[i] = createSlider(0,300,0);
     sliders[i].position(i-50,300);
     sliders[i].style('width','200px');
@@ -50,7 +50,7 @@ function setup() {
 
 function draw() {
 
-  for (var i = 0; i < 140; i += 20) {
+  for (var i = 0; i < 140; i += 20) { //This for loop creates the value for the sliders. And the red yellow and green colors also
     var analyze = fft.analyze();
     var amp = analyze[i];
     var r = map(amp, 0, 256, 0, 300);
@@ -58,7 +58,7 @@ function draw() {
     var red = 0;
     var green = 0;
     var blue = 0;
-    if(r <= 150) {
+    if(r <= 150) { //If statements that decide whether the sliders are red yellow or green.
       red = 0;
       green = 200;
       blue = 0;
